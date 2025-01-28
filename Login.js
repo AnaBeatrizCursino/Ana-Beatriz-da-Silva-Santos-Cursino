e19eaaff280b0f99c28eb7664c72786137b5daa3
Ana Beatriz Cursino <anacursinounivap@gmail.com>
2025-01-28 00:12:25 -0300

    Atualizar o

---
+++ Login.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/users/login', {
+        email,
+        password,
+      });
+      localStorage.setItem('token', response.data.token);
+      alert('Login realizado com sucesso!');
+    } catch {
+      alert('Credenciais inválidas!');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Entrar</button>
+    </div>
+  );
+}
+
+export default Login;
